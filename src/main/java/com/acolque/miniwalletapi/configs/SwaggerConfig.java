e24dc042c011749adae6d10dc76b2185706b1866
Arnold <arnoldclq3@gmail.com>
2023-05-17 16:34:08 -0300

    Add swagger documentation (#7)

---
+++ src/main/java/com/acolque/miniwalletapi/configs/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.acolque.miniwalletapi.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Mini Wallet API -------")
+                        .version("1")
+                        .description("Una mini wallet para poder visualizar las cryptomonedas más conocidas")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
+}
